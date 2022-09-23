5722785ad6bdbe04b78f97dc113dab3a23ab39f4
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-23 11:21:57 -0400

    Initial Commit

---
+++ fructus/fructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  fructusApp.swift
+//  fructus
+//
+//  Created by Silvanei  Martins on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct fructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
